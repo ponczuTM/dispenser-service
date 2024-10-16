7de66b7bc87e0e955e0cdec3a66434a3e59a94e2
ponczuTM <mroczkowskioliwer10@gmail.com>
2024-10-16 08:46:27 +0200

    app

--- dispenser-fe/src/App.jsx
+++ dispenser-fe/src/App.jsx
@@ -5,6 +5,20 @@ const products = [
   { id: 1, category: "burgers", img: "bigmac.png", name: "BigMac" },
   { id: 2, category: "burgers", img: "hamburger.png", name: "Hamburger" },
   { id: 3, category: "burgers", img: "cheeseburger.png", name: "Cheeseburger" },
+  { id: 4, category: "burgers", img: "chikker.png", name: "Chikker" },
+  { id: 5, category: "burgers", img: "jalapenoburger.png", name: "Jalapeno Burger" },
+  { id: 6, category: "burgers", img: "mcchiken.png", name: "McChiken" },
+  { id: 7, category: "burgers", img: "mccrispy-supreme.png", name: "McCrispy Supreme" },
+  { id: 8, category: "burgers", img: "mccrispy.png", name: "McCrispy" },
+  { id: 9, category: "burgers", img: "mcdouble.png", name: "McDouble" },
+  { id: 10, category: "burgers", img: "mcroyal-double.png", name: "McRoyal Double" },
+  { id: 11, category: "burgers", img: "mcroyal.png", name: "McRoyal" },
+  { id: 12, category: "burgers", img: "red-chikker.png", name: "Red Chikker" },
+  { id: 13, category: "burgers", img: "veggie-burger.png", name: "Veggie Burger" },
+  { id: 14, category: "burgers", img: "wiesmac-double.png", name: "Wieśmac Double" },
+  { id: 15, category: "burgers", img: "wiesmac.png", name: "Wieśmac" },
+  { id: 16, category: "mccafe", img: "caramel-latte-macchiato.png", name: "Caramel Latte Macchiato" },
+  { id: 17, category: "mccafe", img: "espresso.png", name: "Espresso" },
 ];
 
 function App() {
@@ -14,8 +28,6 @@ function App() {
       return acc;
     }, {})
   );
-
-  const [isCartOpen, setIsCartOpen] = useState(false);
 
   const increment = (id) => {
     setQuantities((prevQuantities) => ({
@@ -32,7 +44,7 @@ function App() {
   };
 
   const handleOrder = async () => {
-    const orderDetails = { cornerNumber: "125" };
+    const orderDetails = { cornerNumber: "125" }; // Przykładowy numer rogu
 
     try {
       const response = await fetch("http://localhost:5000/send-order", {
@@ -62,10 +74,6 @@ function App() {
     }
   };
 
-  const toggleCart = () => {
-    setIsCartOpen(!isCartOpen);
-  };
-
   const cartItems = products.filter((product) => quantities[product.id] > 0);
   const totalItems = cartItems.reduce(
     (acc, product) => acc + quantities[product.id],
@@ -75,12 +83,11 @@ function App() {
   return (
     <div className="App">
       <h1>Menu</h1>
-
       <table>
         <thead>
           <tr>
-            <th></th>
-            <th></th>
+            <th>Zdjęcie</th>
+            <th>Nazwa</th>
             <th>Ilość</th>
           </tr>
         </thead>
