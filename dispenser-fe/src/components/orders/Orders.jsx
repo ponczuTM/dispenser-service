0ac6c86aff71c3f5ba4215277256e933b28ceeef
ponczuTM <mroczkowskioliwer10@gmail.com>
2024-10-18 13:44:42 +0200

    orders

--- dispenser-fe/src/components/orders/Orders.jsx
+++ dispenser-fe/src/components/orders/Orders.jsx
@@ -4,8 +4,31 @@ import styles from "./Orders.module.css";
 
 const products = [
   { id: 1, category: "burgers", img: "bigmac.png", name: "BigMac" },
-  { id: 2, category: "burgers", img: "hamburger.png", name: "Hamburger" },
+  { id: 2, category:   "burgers", img: "hamburger.png", name: "Hamburger" },
   { id: 3, category: "burgers", img: "cheeseburger.png", name: "Cheeseburger" },
+  { id: 4, category: "burgers", img: "chikker.png", name: "Chikker" },
+  { id: 5, category: "burgers", img: "jalapenoburger.png", name: "Jalapeno Burger" },
+  { id: 6, category: "burgers", img: "mcchiken.png", name: "McChiken" },
+  { id: 7, category: "burgers", img: "mccrispy-supreme.png", name: "McCrispy Supreme" },
+  { id: 8, category: "burgers", img: "mccrispy.png", name: "McCrispy" },
+  { id: 9, category: "burgers", img: "mcdouble.png", name: "McDouble" },
+  { id: 10, category: "burgers", img: "mcroyal-double.png", name: "McRoyal Double" },
+  { id: 11, category: "burgers", img: "mcroyal.png", name: "McRoyal" },
+  { id: 12, category: "burgers", img: "red-chikker.png", name: "Red Chikker" },
+  { id: 13, category: "burgers", img: "veggie-burger.png", name: "Veggie Burger" },
+  { id: 14, category: "burgers", img: "wiesmac-double.png", name: "Wieśmac Double" },
+  { id: 15, category: "burgers", img: "wiesmac.png", name: "Wieśmacx" },
+  { id: 16, category: "mccafe", img: "cafe-latte.png", name: "Cafe Latte" },
+  { id: 17, category: "mccafe", img: "cappuccino.png", name: "Cappuccino" },
+  { id: 18, category: "mccafe", img: "caramel-latte-macchiato.png", name: (<>Choco Latte<br/>Macchiato</>),},
+  { id: 19, category: "mccafe", img: "choco-latte-macchiato.png", name: (<>Choco Latte<br/>Macchiato</>),},
+  { id: 20, category: "mccafe", img: "coffee-black.png", name: "Kawa Czarna" },
+  { id: 21, category: "mccafe", img: "coffee-milk.png", name: "Kawa z Mlekiem" },
+  { id: 22, category: "mccafe", img: "espresso.png", name: "Espresso" },
+  { id: 23, category: "mccafe", img: "flat-white.png", name: "Flat White" },
+  { id: 24, category: "mccafe", img: "iced-caramel-latte.png", name: (<>Iced Latte<br/>Latte</>) },
+  { id: 25, category: "mccafe", img: "iced-latte.png", name: "Iced Latte" },
+  { id: 26, category: "mccafe", img: "iced-raspberry-latte.png", name: (<>Iced Raspberry<br/>Latte</>) },
 ];
 
 const Orders = () => {
